e52b442c8a5d3208c4e56e735f519107a7a8b6c5
omarmohsen179 <mohsenomar350@gmail.com>
2024-06-04 13:26:26 +0300

    enhance code

--- mod/api/resolvers/entities.ts
+++ mod/api/resolvers/entities.ts
@@ -1,12 +1,6 @@
 import { Query } from "lib/types/api";
 import modelFactory from "./factory";
 
-/**
- * Fetches entities based on the model name and query parameters.
- * @param {string} modelName - The name of the model to query.
- * @param {Query} query - The query parameters for fetching entities.
- * @returns {Promise<any>} - The result of the query execution.
- */
 export const getEntities = async (modelName: string, query: Query) => {
   let model = modelFactory(modelName);
   try {
